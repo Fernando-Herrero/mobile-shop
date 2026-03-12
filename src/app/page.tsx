2c24cd39d7079a517d233f2ac57966e06faa1269
Fernando Herrero <herrerofernando.94@gmail.com>
2026-03-12 16:16:55 +0100

    feature(Phone-list): add phone grid to the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
-export default function Home() {
+import PhoneCard from "@/components/phoneCard/PhoneCard";
+import { getProducts } from "@/services/api";
+import { ProductList } from "@/types/products.types";
+
+export default async function Home() {
+    const phones: ProductList[] = await getProducts();
     return (
-        <main>
-            <h1>Mobile Shop</h1>
+        <main className="home">
+            <div className="phones-grid">
+                {phones.map((phone) => (
+                    <PhoneCard key={phone.id} phone={phone} />
+                ))}
+            </div>
         </main>
     );
 }
