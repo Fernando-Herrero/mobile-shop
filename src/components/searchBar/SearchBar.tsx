6e6ce056becd0b3c989d24f40fa599da4f11213f
Fernando Herrero <herrerofernando.94@gmail.com>
2026-03-12 19:17:03 +0100

    feature(search): add styles to search, need refactor to search from Api, not doing rn

--- src/components/searchBar/SearchBar.tsx
+++ src/components/searchBar/SearchBar.tsx
@@ -1,3 +1,4 @@
+import "./SearchBar.css";
 interface SearchBarProps {
     search: string;
     setSearch: (value: string) => void;
@@ -9,16 +10,35 @@ export default function SearchBar({
     setSearch,
     results,
 }: SearchBarProps) {
-    return (
-        <div>
-            <input
-                type="text"
-                value={search}
-                placeholder="Search for a smartphone..."
-                onChange={(event) => setSearch(event.target.value)}
-            />
+    const handleClear = () => setSearch("");
 
-            <span>{results} RESULTS</span>
+    return (
+        <div className="input-container">
+            <div className="input-wrapper">
+                <input
+                    id="search"
+                    type="text"
+                    value={search}
+                    placeholder="Search for a smartphone..."
+                    onChange={(event) => setSearch(event.target.value)}
+                    className="input-search"
+                />
+                {search && (
+                    <button
+                        className="clear-btn"
+                        onClick={handleClear}
+                        aria-label="Clear search"
+                    >
+                        {/* eslint-disable-next-line @next/next/no-img-element */}
+                        <img
+                            src="/icons/close-small.svg"
+                            alt="x close button"
+                        />
+                    </button>
+                )}
+            </div>
+
+            <span className="search-results">{results} RESULTS</span>
         </div>
     );
 }
